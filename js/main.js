395d9acd81bcc8ce17bc911951a2731f77ca447d
Olya Yakymovych <msolya@outlook.com>
2017-12-13 00:59:48 +0200

    added lightbox

--- js/main.js
+++ js/main.js
@@ -20,3 +20,61 @@ function getCurrentScroll() {
     return window.pageYOffset || document.documentElement.scrollTop;
     }
 });
+
+
+$(document).ready(function() {
+    const setActive = function() {
+        const self = $(this);
+        const listMenu = $('.navbar-nav li');
+        let curIndex = -1;
+
+        $('.nav-header').each(function(index) {
+            if (self.scrollTop() > $(this).offset().top - 70) {
+                curIndex = index;
+            }
+        });
+
+        if (curIndex === -1) {
+            listMenu.removeClass('active');
+        }
+        else {
+            const curItemMenu = listMenu.eq(curIndex);
+
+            if (!curItemMenu.hasClass('active')) {
+                curItemMenu.addClass('active');
+                curItemMenu.siblings().removeClass('active');
+            }
+        }
+    };
+
+    setActive();
+
+    $('.navbar-nav li').on('click', function(e) {
+        e.preventDefault();
+
+        const index = $(this).index();
+        const curHeader = $('.section-js').eq(index).offset().top;
+
+        $('html, body').animate({
+            scrollTop: curHeader - 70
+        }, 1000);
+    });
+
+    $(document).on('scroll', function() {
+        const posDoc = $(this).scrollTop();
+
+        $('.section-js').each(function(index, item) {
+            const topHeader = $(this).offset().top - 70;
+            const botHeader = topHeader + $(this).height();
+
+            if (
+                posDoc > topHeader &&
+                posDoc < botHeader
+            ) {
+                $('.navbar-nav li').removeClass('active');
+                $('.navbar-nav li').eq(index).addClass('active');
+            }
+
+        });
+    });
+});
